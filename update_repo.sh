b608afa503043277d782412ca8fa89fd3d01595c
HossamMamd0uh <hossammamdouh8010@gmail.com>
2020-09-30 19:21:13 +0200

    ver2

--- update_repo.sh
+++ update_repo.sh
@@ -9,13 +9,14 @@ var=$(git log -1 --pretty=format:%s)
 if [ $var == $commit ]
 then
     echo "Everything is updated"
-else
+elif [ $var != $commit ]
+then
     git commit -m $commit
     git push
     echo "All pushed"
-fi
+else
     echo "Something went wrong, please check the log file that I'll generate"
     echo "Generating the file..."
     git log > git-log.txt
     echo "Git log stored in git-log.txt"
-
+fi
